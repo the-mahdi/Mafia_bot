0ecb1df1271f8eb444de99b5a4b7dda10fcf8baa
Mohammadmahdi Rahimiasl <mohammadmahdi.rahimiasl@uantwerpen.be>
2024-12-19 02:46:21 +0100

    Block users from joining a game if it's already started

--- handlers/game_management.py
+++ handlers/game_management.py
@@ -319,26 +319,32 @@ async def join_game(update: ContextTypes.DEFAULT_TYPE, context: ContextTypes.DEF
     user_id = update.effective_user.id
     username = context.user_data.get("username", f"User{user_id}")
 
-    cursor.execute("SELECT game_id, moderator_id FROM Games WHERE passcode = ?", (passcode,))
+    cursor.execute("SELECT game_id, moderator_id, started FROM Games WHERE passcode = ?", (passcode,))
     result = cursor.fetchone()
     if result:
-        game_id, moderator_id = result
+        game_id, moderator_id, started = result
+
+        if started:
+            logger.debug(f"Attempt to join started game_id: {game_id}")
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Cannot join. The game has already started.")
+            return  # Exit the function to prevent joining
+
         context.user_data['game_id'] = game_id  # Store game_id in user_data
         logger.debug(f"User joined game. game_id stored in user_data: {game_id}")
 
         # Update or insert user information
         cursor.execute("""
-            INSERT INTO Users (user_id, username, last_updated)
-            VALUES (?, ?, CURRENT_TIMESTAMP)
-            ON CONFLICT(user_id) DO UPDATE SET
-            username = ?,
-            last_updated = CURRENT_TIMESTAMP
-            WHERE username != ? OR last_updated < CURRENT_TIMESTAMP
+        INSERT INTO Users (user_id, username, last_updated)
+        VALUES (?, ?, CURRENT_TIMESTAMP)
+        ON CONFLICT(user_id) DO UPDATE SET
+        username = ?,
+        last_updated = CURRENT_TIMESTAMP
+        WHERE username != ? OR last_updated < CURRENT_TIMESTAMP
         """, (user_id, username, username, username))
 
         cursor.execute("""
-            INSERT OR IGNORE INTO Roles (game_id, user_id, role)
-            VALUES (?, ?, NULL)
+        INSERT OR IGNORE INTO Roles (game_id, user_id, role)
+        VALUES (?, ?, NULL)
         """, (game_id, user_id))
 
         conn.commit()
@@ -355,6 +361,7 @@ async def join_game(update: ContextTypes.DEFAULT_TYPE, context: ContextTypes.DEF
     else:
         message = "Invalid passcode. Please try again."
         await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
 
 async def start_game(update: ContextTypes.DEFAULT_TYPE, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.debug("Starting the game.")
