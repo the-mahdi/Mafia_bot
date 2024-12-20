75e313d3acac49c500739817f7b7acefee7fdbd5
Mohammadmahdi Rahimiasl <mohammadmahdi.rahimiasl@uantwerpen.be>
2024-12-20 08:26:52 +0100

    added imports to handlers and handlers.gamemanagement inits

---
+++ src/handlers/__init__.py
@@ -0,0 +1,27 @@
+# src/handlers/__init__.py
+from .button_handler import (
+    button_handler,
+    final_confirm_vote_handler,
+    cancel_vote_handler,
+    handle_button,
+    show_manage_games_menu,
+    handle_maintainer_confirmation
+)
+from .passcode_handler import passcode_handler, handle_passcode, handle_template_confirmation, save_template_as_pending, is_valid_passcode
+from .start_handler import start_handler, start
+
+__all__ = [
+    "button_handler",
+    "final_confirm_vote_handler",
+    "cancel_vote_handler",
+    "handle_button",
+    "show_manage_games_menu",
+    "handle_maintainer_confirmation",
+    "passcode_handler",
+    "handle_passcode",
+    "handle_template_confirmation",
+    "save_template_as_pending",
+    "is_valid_passcode",
+    "start_handler",
+    "start"
+]
---
+++ src/handlers/game_management/__init__.py
@@ -0,0 +1,41 @@
+from .base import get_random_shuffle, get_player_count, get_templates_for_player_count
+from .create_game import create_game
+from .elimination import eliminate_player, handle_elimination_confirmation, confirm_elimination, cancel_elimination
+from .join_game import join_game
+from .roles_setup import set_roles, show_role_buttons, confirm_and_set_roles
+from .start_game import start_game, start_latest_game
+from .voting import (
+    announce_voting,
+    announce_anonymous_voting,
+    handle_vote,
+    confirm_votes,
+    final_confirm_vote,
+    cancel_vote,
+    send_voting_summary,
+    process_voting_results
+)
+
+__all__ = [
+    "get_random_shuffle",
+    "get_player_count",
+    "get_templates_for_player_count",
+    "create_game",
+    "eliminate_player",
+    "handle_elimination_confirmation",
+    "confirm_elimination",
+    "cancel_elimination",
+    "join_game",
+    "set_roles",
+    "show_role_buttons",
+    "confirm_and_set_roles",
+    "start_game",
+    "start_latest_game",
+    "announce_voting",
+    "announce_anonymous_voting",
+    "handle_vote",
+    "confirm_votes",
+    "final_confirm_vote",
+    "cancel_vote",
+    "send_voting_summary",
+    "process_voting_results"
+]
